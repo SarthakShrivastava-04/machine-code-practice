ed64ea85beffd77832aacdfc065d187179236235
Sarthak Shrivastava <shrivastavasarthak18@gmail.com>
2025-07-12 02:56:16 +0530

    feat: connect mongodb

---
+++ todo-mongo/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const URL = process.env.MONGO_URL;
+
+export default connectDB = async() => {
+   try{
+    await mongoose.connect(URL);
+    console.log("MongoDB connected successfully");
+   }
+   catch(err){
+    console.error("MongoDB connection failed:", err);
+   }
+};
