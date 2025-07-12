59e1c599f4a6e7ef6021ea35200aad1a6d983bcf
Sarthak Shrivastava <shrivastavasarthak18@gmail.com>
2025-07-12 16:09:22 +0530

    implement auth protection middleware

---
+++ todo-mongo/middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export default function protect(req, res) {
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token)
+    return res.status(401).json({ message: "No token, unauthorized" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
